35d1c6b4301516ba443e1dcf288042ba8b062702
TremorSongg <jua.riverag@duocuc.cl>
2025-10-29 00:35:31 -0300

    removed body from layout
    unused

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,3 @@
-import { Body } from "../components/Body";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import { Outlet } from "react-router-dom";
